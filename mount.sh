da19f86c28f4234be8ec3a0c28ec8e26d9013b61
Moritz Eysholdt <moritz@gitpod.io>
2024-04-25 09:26:24 +0000

    imporved mount script

--- mount.sh
+++ mount.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 
+SHARED_FOLDER=/workspace/shared
+S3_BUCKET=shared-state-test
+
+sudo mkdir "$SHARED_FOLDER"
+sudo chown -R gitpod:gitpod "$SHARED_FOLDER"
+s3fs "$S3_BUCKET" "$SHARED_FOLDER"
+
+mkdir "$SHARED_FOLDER/secretsauce"
+ln -s "$SHARED_FOLDER/secretsauce" /workspace/m2-repository/com/mycompany/secretsauce
